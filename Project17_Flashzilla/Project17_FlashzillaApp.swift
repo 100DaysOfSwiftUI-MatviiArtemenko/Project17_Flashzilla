91585be1aa3976ea9ce5d262c78ac53394795577
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2023-03-08 15:56:53 +0000

    Initial Commit

---
+++ Project17_Flashzilla/Project17_FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project17_FlashzillaApp.swift
+//  Project17_Flashzilla
+//
+//  Created by admin on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project17_FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
